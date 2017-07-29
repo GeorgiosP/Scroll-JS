c80a1cfce8ef8422115e651b97f53d7bb2019906
GeorgiosP <g.psarakis3800@gmail.com>
2017-07-29 05:45:03 -0400

    adding inital scroll.js will be making some changes

---
+++ scroll.js
@@ -0,0 +1,37 @@
+function getElementY(query) {
+  return window.pageYOffset + document.querySelector(query).getBoundingClientRect().top
+}
+
+function doScrolling(element, duration) {
+  var startingY = window.pageYOffset
+  var elementY = getElementY(element)
+  // If element is close to page's bottom then window will scroll only to some position above the element.
+  var targetY = document.body.scrollHeight - elementY < window.innerHeight ? document.body.scrollHeight - window.innerHeight : elementY
+  var diff = targetY - startingY
+  // Easing function: easeInOutCubic
+  // From: https://gist.github.com/gre/1650294
+  var easing = function (t) {
+    return t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1
+  }
+  var start
+
+  if (!diff) return
+
+  // Bootstrap our animation - it will get called right before next frame shall be rendered.
+  window.requestAnimationFrame(function step(timestamp) {
+    if (!start) start = timestamp
+    // Elapsed milliseconds since start of scrolling.
+    var time = timestamp - start
+    // Get percent of completion in range [0, 1].
+    var percent = Math.min(time / duration, 1)
+    // Apply the easing.
+    percent = easing(percent)
+
+    window.scrollTo(0, startingY + diff * percent)
+
+    // Proceed with animation as long as we wanted it to.
+    if (time < duration) {
+      window.requestAnimationFrame(step)
+    }
+  })
+}
